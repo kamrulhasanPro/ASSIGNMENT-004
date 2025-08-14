b031e14437facf09f3ce4e2164ffec0857f4c19a
kamrulmiazi <kamrul116522@gmail.com>
2025-08-14 12:26:58 +0600

    problem5 solved

---
+++ problem5.js
@@ -0,0 +1,26 @@
+//* Problem-05: Exam Result Report Generator
+function  resultReport( marks ) {
+    // You have to write your code here
+    if(!Array.isArray(marks)){
+        return "Invalid"
+    }
+
+    let totalMarks = 0;
+    let passCount = 0;
+    let failCount = 0;
+
+    for(let mark of marks){
+        totalMarks += mark;
+        if(mark >= 40){
+            passCount++;
+        }else{
+            failCount++
+        }
+    } ;
+
+    let average = Math.round(totalMarks / marks.length) || 0 ;
+    let result = {finalScore: average, pass: passCount, fail: failCount};
+    return result;
+}
+
+console.log(resultReport([98, 87, 67, 91, 92, 33, 87]))
