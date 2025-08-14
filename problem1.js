bf6cd1c577495384c0dec2ce85b1f810bd4389ae
kamrulmiazi <kamrul116522@gmail.com>
2025-08-14 10:46:01 +0600

    problem1 solved

---
+++ problem1.js
@@ -0,0 +1,12 @@
+//* Problem-01 : Train TT's Fine Calculator
+function totalFine(fare) {
+  // You have to write your code here
+  if (typeof fare !== "number" || fare <= 0) {
+    return "Invalid";
+  }
+
+  let fareCalculate = fare + (fare * 20) / 100 + 30;
+  return fareCalculate;
+}
+
+console.log(totalFine("Gorib tai ticket katinai"));
